e00da38801ad32f8348b66e76caf75508c356128
Karolina Nowak <nowak.karolina90@gmail.com>
2019-04-02 21:16:34 +0200

    Change script.js by adding functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
-var computerMove, randomNumber;
+var moveId, computerMove, playerMove, randomNumber, playerInput;
+
+function getMoveName(moveId) {
+    console.log('wywołano funkcję getMoveName z argumentem: ' + moveId);
+    if (moveId == 1) {
+        return 'kamień';
+    } else if (moveId == 2) {
+        return 'papier';
+    } else if (moveId == 3) {
+        return 'nożyce';
+    } else {
+        printMessage('Nie znam ruchu "' + moveId + '". Zakładam, że chodziło o "kamień".');
+        return 'kamień';
+    }
+}
+
+function displayResult(playerMove, computerMove) {
+    console.log('wywołano funkcję displayResults z argumentami: ' + playerMove + ', ' + computerMove);
+    if (playerMove == 'kamień' && computerMove == 'nożyce') {
+        printMessage('Wygrywasz!');
+    } else if (playerMove == 'papier' && computerMove == 'kamień') {
+        printMessage('Wygrywasz!');
+    } else if (playerMove == 'nożyce' && computerMove == 'papier') {
+        printMessage('Wygrywasz!');
+    } else if (playerMove == computerMove) {
+        printMessage('Remis.');
+    } else {
+        printMessage('Przegrywasz :(');
+    }
+    printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove + '.');
+}
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('wybór ruchu gracza to: ' + playerInput);
+playerMove = getMoveName(playerInput);
+console.log('ruch gracza to: ' + playerMove);
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-    computerMove = 'kamień';
-} else if (randomNumber == '2') {
-    computerMove = 'papier';
-} else {
-    computerMove = 'nożyce';
-}
-printMessage('Mój ruch: ' + computerMove);
+computerMove = getMoveName(randomNumber);
+console.log('ruch komputera to: ' + computerMove);
+displayResult(playerMove, computerMove);

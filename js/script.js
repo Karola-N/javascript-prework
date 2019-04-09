68db4241d67989d7f1880fe69e229b8b8b85acee
Karolina Nowak <nowak.karolina90@gmail.com>
2019-04-09 21:16:24 +0200

    Remove var buttonName in script.js

--- js/script.js
+++ js/script.js
@@ -1,57 +1,8 @@
-var moveId, computerMove, playerMove, randomNumber, playerInput;
-/**
-function getMoveName(moveId) {
-    console.log('wywołano funkcję getMoveName z argumentem: ' + moveId);
-    if (moveId == 1) {
-        return 'kamień';
-    } else if (moveId == 2) {
-        return 'papier';
-    } else if (moveId == 3) {
-        return 'nożyce';
-    } else {
-        printMessage('Nie znam ruchu "' + moveId + '". Zakładam, że chodziło o "kamień".');
-        return 'kamień';
-    }
-}
-
-function displayResult(playerMove, computerMove) {
-    console.log('wywołano funkcję displayResults z argumentami: ' + playerMove + ', ' + computerMove);
-    if (playerMove == 'kamień' && computerMove == 'nożyce') {
-        printMessage('Wygrywasz!');
-    } else if (playerMove == 'papier' && computerMove == 'kamień') {
-        printMessage('Wygrywasz!');
-    } else if (playerMove == 'nożyce' && computerMove == 'papier') {
-        printMessage('Wygrywasz!');
-    } else if (playerMove == computerMove) {
-        printMessage('Remis.');
-    } else {
-        printMessage('Przegrywasz :(');
-    }
-    printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove + '.');
-}
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
-*/
-
-
-
-
-
-
-
-
-var buttonName, buttonRock, buttonPaper, ButtonScissors;
+var buttonRock, buttonPaper, buttonScissors;
 
 buttonRock = document.getElementById('button-rock');
 buttonPaper = document.getElementById('button-paper');
-ButtonScissors = document.getElementById('button-scissors');
+buttonScissors = document.getElementById('button-scissors');
 
 function buttonClicked(buttonName) {
     clearMessages();
@@ -94,4 +45,4 @@ function buttonClicked(buttonName) {
 
 buttonRock.addEventListener('click', function() { buttonClicked('kamień'); });
 buttonPaper.addEventListener('click', function() { buttonClicked('papier'); });
-ButtonScissors.addEventListener('click', function() { buttonClicked('nożyce'); });
+buttonScissors.addEventListener('click', function() { buttonClicked('nożyce'); });
